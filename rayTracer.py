c4fe75c0685e1f509b673b83a8ea9e79954d62f7
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-10-15 00:13:03 -0600

    Cylinder, basic scene

--- rayTracer.py
+++ rayTracer.py
@@ -1,89 +1,61 @@
-# from gl import Raytracer, color
-# from obj import Obj, Texture, Envmap
-# from sphere import *
-
-
-# gray = Material(diffuse = color(0.4, 0.4, 0.4 ), spec = 64)
-# metallicSeaweed = Material(diffuse = color(0.031, 0.255, 0.361), spec = 64)
-# etonBlue = Material(diffuse = color(0.537, 0.808, 0.580), spec = 64)
-# wall = Material(diffuse = color(0.996, 0.996, 0.996), spec = 64)
-# floor = Material(diffuse = color(0.851, 0.851, 0.851), spec = 64)
-
-
-
-# rayTracer = Raytracer(500, 500)
-
-
-# rayTracer.pointLight = PointLight(position = (0,0,0), intensity = 1)
-# rayTracer.ambientLight = AmbientLight(strength = 0.1)
-
-
-# # Cuarto
-# rayTracer.scene.append( Plane((0, -3, 0), (0,1,0), wall)) # Pared izquierda
-# rayTracer.scene.append( Plane((0, 0, -10), (0,0,1), gray)) # Pared enfrente 
-# rayTracer.scene.append( Plane(( 3, 0,0), (-1,0,0), wall)) # Pared derecha
-# rayTracer.scene.append( Plane(( -3,0, 0), (1,0,0), floor)) # Suelo
-# rayTracer.scene.append( Plane((0, 3, 0), (0,-1,0), wall) ) # Techo 
-
-# # Cubos
-# rayTracer.scene.append(AABB((-1, -2.3, -7), 1, metallicSeaweed))
-# rayTracer.scene.append(AABB((1, -2.3, -7), 1, etonBlue))
-
-
-# rayTracer.rtRender()
-# rayTracer.glFinish('output.bmp')
-
-# print("El bitmap se ha generado con exito, revisa la carpeta contenedora")
-
-
 from gl import Raytracer, color
 from obj import Obj, Texture, Envmap
 from sphere import *
 import random
 
-print("Cargando ...")
+print("\nCargando ...\n")
 
-brick = Material(diffuse = color(0.8, 0.25, 0.25 ), spec = 16)
+brick = Material(diffuse = color(0, 0, 0 ), spec = 16)
 stone = Material(diffuse = color(0.4, 0.4, 0.4 ), spec = 32)
 mirror = Material(spec = 64, matType = REFLECTIVE)
 glass = Material(spec = 64, ior = 1.5, matType= TRANSPARENT) 
 
+# c2a472 - 194, 164, 114
+colosseum1 = Material(diffuse = color(0.761, 0.643, 0.447), spec = 16)
+# 9a7445 - 154, 116, 69
+colosseum2 = Material(diffuse = color(0.604, 0.455, 0.271), spec = 16)
+# b88f57 - 184, 143, 87
+colosseum3 = Material(diffuse = color(0.722, 0.561, 0.341), spec = 16)
+# feda9c - 254, 218, 156
+colosseum4 = Material(diffuse = color(0.996, 0.855, 0.612), spec = 16)
+
 boxMat = Material(texture = Texture('box.bmp'))
+sanpietrini = Material(texture = Texture('sanpietrini.bmp'))
 
 earthMat = Material(texture = Texture('earthDay.bmp'))
 
 
-width = 512
-height = 512
-r = Raytracer(width,height)
+r = Raytracer(736, 512)
 r.glClearColor(0.2, 0.6, 0.8)
 r.glClear()
 
-r.envmap = Envmap('envmap.bmp')
+r.envmap = Envmap('env2.bmp')
 
 # Lights
 #r.pointLights.append( PointLight(position = V3(-4,4,0), intensity = 0.5))
 #r.pointLights.append( PointLight(position = V3( 4,0,0), intensity = 0.5))
-r.dirLight = DirectionalLight(direction = (1, -1, -2), intensity = 0.5)
-r.ambientLight = AmbientLight(strength = 0.1)
-
-# Objects
-#r.scene.append( Sphere(V3( 0, 0, -8), 2, brick) )
-#r.scene.append( Sphere(V3( -0.5, 0.5, -5), 0.25, stone))
-#r.scene.append( Sphere(V3( 0.25, 0.5, -5), 0.25, stone))
+# r.dirLight = DirectionalLight(direction = (1, -1, -2), intensity = 1)
+r.ambientLight = AmbientLight(strength = 1)
 
 
-r.scene.append( AABB((0, -3, -10), (5, 0.1, 5) , boxMat ) )
-#r.scene.append( AABB(V3(1.5, 1.5, -5), V3(1, 1, 1) , boxMat ) )
-#r.scene.append( AABB(V3(-1.5, 0, -5), V3(1, 1, 1) , boxMat ) )
+r.scene.append( AABB((0, -3, -20), (40, 0.1, 40) , brick))
+r.scene.append( AABB((0, -1, -30), (50, 5, 0.1) , stone))
 
-r.scene.append( Sphere(( 0, 0, -8), 2, earthMat))
+r.scene.append( AABB((0, 0, -10), (1, 1.4, 0) , colosseum1))
+r.scene.append( AABB((0, 0.73, -9.9), (1, 0.5, 0) , colosseum2))
+r.scene.append( Sphere(( 0, 0.3, -6), 0.2, colosseum1))
+
+# r.scene.append( Plane(( 0, -10, -20), (0, 1, 0.01), stone)) # Suelo
+
+# r.scene.append(Cylinder(0.15, 1.8, (0, 1, -10), colosseum1))
 
 
 
 r.rtRender()
 
 r.glFinish('output.bmp')
+
+print("El bitmap se ha generado con exito, revisa la carpeta contenedora")
 
 
 
--- sphere.py
+++ sphere.py
@@ -2,6 +2,8 @@ from gl import color
 from mathLib import * 
 from numpy import arccos, arctan2
 
+from math import sqrt, inf
+
 
 OPAQUE = 0
 REFLECTIVE = 1
@@ -181,3 +183,95 @@ class AABB(object):
                          normal = intersect.normal,
                          texCoords = uvs,
                          sceneObject = self)
+
+
+# https://github.com/tobycyanide/pytracer/blob/master/raytracer/shapes.py
+class Cylinder(object):
+
+    def __init__(self, radius, height, center, material):
+        self.radius = radius
+        self.height = height
+        self.closed = False
+        self.center = center
+        self.material = material
+
+    def ray_intersect(self, origin, direction):
+        a = direction[0] ** 2 + direction[2] ** 2
+        if abs(a) < 0.0001: #epsilon = 0.0001
+            return None
+
+        b = 2 * (
+            direction[0] * (origin[0] - self.center[0])
+            + direction[2] * (origin[2] - self.center[2])
+        )
+        c = (
+            (origin[0] - self.center[0]) ** 2
+            + (origin[2] - self.center[2]) ** 2
+            - (self.radius ** 2)
+        )
+
+        discriminant = b ** 2 - 4 * (a * c)
+
+        if discriminant < 0.0:
+            return None
+
+        t0 = (-b - sqrt(discriminant)) / (2 * a)
+        t1 = (-b + sqrt(discriminant)) / (2 * a)
+
+        if t0 > t1:
+            t0, t1, t1, t0
+
+        y0 = origin[1] + t0 * direction[1]
+        if self.center[1] < y0 and y0 <= (self.center[1] + self.height):
+            hit = sumVectors(origin, multiply(t0, direction))
+            normal = frobeniusNorm(subVectors(hit, self.center))
+            return Intersect(distance = t0,
+                             point = hit,
+                             normal = normal,
+                             texCoords = None,
+                             sceneObject = self)
+
+        y1 = origin[1] + t1 * direction[1]
+        if self.center[1] < y1 and y1 <= (self.center[1] + self.height):
+            hit = sumVectors(origin, multiply(t1, direction))
+            normal = frobeniusNorm(subVectors(hit, self.center))
+            return Intersect(distance=t1,
+                             point = hit,
+                             normal = normal,
+                             texCoords = None,
+                             sceneObject = self)
+
+        return self.intersect_caps(origin, direction)
+
+    def check_cap(self, origin, direction, t):
+        x = origin[0] + t * direction[0]
+        z = origin[2] + t * direction[2]
+        return (x ** 2 + z ** 2) <= abs(self.radius)
+
+    def intersect_caps(self, origin, direction):
+        if self.closed == False or abs(direction[1]) < 0.0001: #epsilon = 0.0001
+            return None
+
+        t_lower = (self.center[1] - origin[1]) / direction[1]
+        if self.check_cap(origin, direction, t_lower):
+            hit = sumVectors(origin, multiply(t_lower, direction))
+            normal = frobeniusNorm(subVectors(hit, self.center))
+
+            return Intersect(distance = t_lower,
+                             point = hit,
+                             normal = normal,
+                             texCoords = None,
+                             sceneObject = self)
+
+        t_upper = ((self.center[1] + self.height) - origin[1]) / direction[1]
+        if self.check_cap(origin, direction, t_upper):
+            hit = sumVectors(origin, multiply(t_upper, direction))
+            normal = frobeniusNorm(subVectors(hit, self.center))
+
+            return Intersect(distance = t_upper,
+                             point=hit,
+                             normal=normal,
+                             texCoords = None,
+                             sceneObject = self)
+
+        return None
